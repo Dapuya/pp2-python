ad2a80ab1f4e316ddcc3a283723b1968f8dbb08d
Dapuya <78230285+Dapuya@users.noreply.github.com>
2021-03-03 01:08:09 +0600

    upd

--- 4lab/ex.py
+++ 4lab/ex.py
@@ -1,7 +1,30 @@
 import re
 
-f = open('row.txt', 'r')
+f = open('./raw.txt', 'r')
 text = f.read()
 
-patt = re.compile(r'')
+name = re.search(r'ФИЛИАЛ\sТОО\s\w+\s\w+', text)
+bin_number = re.search(r'\d{12}',text)
 
+'''
+title = re.compile(r'')
+cout = re.compile(r'')
+unit_price = re.compile(r'')
+total_price = re.compile(r'')
+date = re.compile(r'')
+addres = re.compile(r'')
+'''
+print("1. Name of the company: ", + name.group())
+print("2. BIN number: ", + bin_number.group())
+
+'''
+Name of the company
+BIN number 
+For each item:
+Title —— (Натрия хлорид 0,9%, 200 мл, фл)
+Cout ——  (2)
+Unit price —— (154,00)
+Total price —— (308,00)
+Date —— (18.04.2019 11:13:58)
+Address —— (г. Нур-султан,Казахстан, Мангилик Ел,19, нп-5)
+'''
