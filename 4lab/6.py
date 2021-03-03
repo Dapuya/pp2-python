e23e5ebc28623014069b1ce2f98fdd59878e9e52
Dapuya <78230285+Dapuya@users.noreply.github.com>
2021-03-03 21:00:26 +0600

    regex

--- 4lab/6.py
+++ 4lab/6.py
@@ -1,14 +1,9 @@
 import re
 
 n = int(input())
-valid = re.compile(r'^[A-Za-z]+[" "]{1,}<[A-Za-z1-9]+[-,.,_]?[A-Za-z1-9]+[@][a-z]+\.[a-z]{1,3}>$')
-
 while n!=0:
-    a = str(input())
-    if valid.findall(a):
-        z = valid.findall(a)
-        for i in z:
-            print(i)
-    else:
-        print()
+    a = input()
+    pat = re.search(r'[\w]+\s<[a-z][\w\.\-]*@[a-z]+[\.][a-z]{1,3}>$', a)
+    if pat:
+        print(a)
     n-=1
--- 4lab/7.py
+++ 4lab/7.py
@@ -1,22 +1,10 @@
 import re
 
-f = open ('raw.txt', 'r')
-text = f.read()
+a = int(input())
 
-name = re.search(r'Филиал\sТОО\s\w+\s\w+', text)
-bin_num = re.search(r'\d{12}', text)
-item = re.findall(r'\d+\.\n(.*)', text)
-cnt = re.findall(r'(\d),\d{3}', text)
-unit_price = re.findall(r'x\s([\d\s]+,\d+)',text)
-total_price = re.findall(r'([\d\s]+,\d+)', text)
-date = re.search(r'\d{2}\.\d{2}\.\d{4}\s\d{2}:\d{2}:\d{2}', text)
-address = re.search(r'г\.\s[\w\-]+,\w+,[\w+\s]+,\d+,\s\w+\-\d+', text)
-print("1. Name of the company: ", name.group())
-print("2. BIN number: ", bin_num.group())
-print("3. Items: ")
-for i in range(len(item)):
-    print("    Title: ",  item[i])
-    print("     Cnt: ",  cnt[i])
-    print("      Unit price: ", unit_price[i])
-print("4. Date ", date.group())
-print("5. Address ", address.group())
+for i in range(0,a):
+    s = input()
+    pattern = re.findall(r'^#[A-Fa-f0-9]{3|6}',s)
+    if pattern:
+        print(pattern.group())
+        
