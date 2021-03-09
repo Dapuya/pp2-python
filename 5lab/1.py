11c749c134bd5a79f40bc5bd02ee3f8df9bb208b
Dapuya <78230285+Dapuya@users.noreply.github.com>
2021-03-09 16:13:12 +0600

    upd

--- 5lab/1.py
+++ 5lab/1.py
@@ -2,11 +2,10 @@ import os
 import shutil
 
 print("1. Current location: ", os.getcwd())
-print("2. Choose one of the options:  ")
-print("    1 - working with file")
-print("    2 - working with directory")
-n = int(input())
-if n == 1 :
+print("2. Work with: 1 -- directory")
+print("              2 -- file")
+x = input()
+if x == '2':
     print("Choose one of the options: ")
     print("  a) delete file")
     print("  b) rename file")        
@@ -15,6 +14,38 @@ if n == 1 :
     print("  e) return to the parent directory")     
     a = input()
     if a == 'a':
+        os.remove(os.remove(x))
+    if a == 'b':
+        os.rename(x, input("enter new name: "))
+    if a == 'c':
+        with open(x, 'a') as f:
+            f.write(input("enter content, that will be added: "))
+    if a == 'd':
+        with open(x, 'w') as f:
+            f.write(input("enter the rewritable content: "))
+    #if a == 'e':
+
+if x == '1':
+    print("Choose one of the options: ")
+    print("  a) rename directory")
+    print("  b) print number of files")        
+    print("  c) print number of directories")        
+    print("  d) list content of the directory")        
+    print("  e) add file to this directory")  
+    print("  f) add new directory to this directory")  
+    d = input()
+    if d == 'a':
+        os.rename(os.remove(x))
+    if d == 'b':
+        d = list(os.listdir(x))
+        print(len(d))
+    # if d == 'c':
+        
+    # if d == 'd':    
+    # if d == 'e':
+    # if d == 'f':
+    
+
         
         
 
