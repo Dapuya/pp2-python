f5e372394d612f2691ffeef4e6434c033597590c
Dapuya <78230285+Dapuya@users.noreply.github.com>
2021-05-20 13:43:15 +0600

    upd

--- 3lab/high.py
+++ 3lab/high.py
@@ -1,11 +1,12 @@
 a = list(input().split(','))
-sas = [0,]
+b = [0,]
+s = 0
+mx = -10000
 for i in range(len(a)):
-    sas.append(int(a[i]))
-cnt = -101
-k=[0,gain[0]]
-for i in range(1,len(gain)):
-    k.append(gain[i]+gain[0])
-    print(k)
-    gain[0]=gain[i]+gain[0]
-return max(k)
+    if i == len(a):
+        break
+    else:
+        s += int(a[i])
+        b.append(s)
+
+print(max(b))
